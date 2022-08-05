72290914ce531210ccc7409b0ab16ad6afc78f0e
Panagiotis Toumpass <ptoumpas@arx.net>
2022-08-05 17:33:58 +0300

    Creating Register Screen

--- app/src/main/java/gr/dipae/hardofhearingcalls/ui/onboarding/compose/RegisterComposables.kt
+++ app/src/main/java/gr/dipae/hardofhearingcalls/ui/onboarding/compose/RegisterComposables.kt
@@ -1,10 +1,49 @@
 package gr.dipae.hardofhearingcalls.ui.onboarding.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import gr.dipae.hardofhearingcalls.R
+import gr.dipae.hardofhearingcalls.ui.base.compose.VerticalSpacerDefault
+import gr.dipae.hardofhearingcalls.ui.base.compose.VerticalSpacerDouble
 import gr.dipae.hardofhearingcalls.ui.onboarding.RegisterViewModel
-import gr.dipae.hardofhearingcalls.ui.splash.SplashViewModel
+import gr.dipae.hardofhearingcalls.ui.theme.hardOfHearingCallsTypography
+import gr.dipae.hardofhearingcalls.ui.theme.primaryColor
 
 @Composable
 fun RegisterScreen(viewModel: RegisterViewModel) {
 
+}
+
+@Composable
+fun RegisterContent() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(primaryColor),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = stringResource(R.string.register_welcome),
+            style = hardOfHearingCallsTypography.h1
+        )
+        VerticalSpacerDefault()
+        Text(
+            text = stringResource(R.string.register_welcome_sub),
+            style = hardOfHearingCallsTypography.h3
+        )
+        VerticalSpacerDouble()
+        Text(
+            text = stringResource(R.string.),
+            style = hardOfHearingCallsTypography.h3
+        )
+    }
 }
--- app/src/main/java/gr/dipae/hardofhearingcalls/ui/theme/Colors.kt
+++ app/src/main/java/gr/dipae/hardofhearingcalls/ui/theme/Colors.kt
@@ -2,10 +2,10 @@ package gr.dipae.hardofhearingcalls.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryColor = Color(0xFF2196F3)
-val primaryDarkColor = Color(0xFF283593)
-val secondaryColor = Color(0xFFFFC107)
-val secondaryDarkColor = Color(0xFFF9A825)
+val primaryColor = Color(0xFFB1C1CC)
+val primaryDarkColor = Color(0xFFB1B3CC)
+val secondaryColor = Color(0xFFCCBCB1)
+val secondaryDarkColor = Color(0xFF674B36)
 val backgroundColor = Color(0xFFFFFFFF)
 val surfaceColor = Color(0xFFFFFFFF)
 val errorColor = Color(0xFFB71C1C)
